7d8a5bee0f7e7369e115ec0f8d4cb20ce380f390
AJ5025 <32069762+AJ5025@users.noreply.github.com>
2018-11-09 23:41:38 +0000

    Merge with DISCO-1

---
+++ DaveryProjectMain/Assets/Scripts/BulletController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+    public float speed = 20f;
+    public Rigidbody2D rb2d;
+    public new Collider2D collider;
+
+	// Use collision.name to find what has been collided with
+
+	void Start () {
+        rb2d.velocity = transform.right * speed;
+        gameObject.layer = 9;
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Physics2D.IgnoreCollision(collision.collider, collider);
+    }
+    
+}
---
+++ DaveryProjectMain/Assets/Scripts/DaveController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DaveController : MonoBehaviour {
+
+    public Transform FirePoint;
+    public GameObject bulletPrefab;
+
+    private int thisCharId;
+    private GameObject UI;
+    private LevelManager level;
+    private int currentCharId;
+
+    private void Start()
+    {
+        UI = GameObject.Find("UI");
+        thisCharId = GetComponent<CharacterController>().thisCharId;
+    }
+    // Update is called once per frame
+    void Update () {
+        level = UI.GetComponent<LevelManager>();
+        currentCharId = level.currentCharId;
+
+        if (currentCharId == thisCharId)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
+        }
+	}
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, FirePoint.position, FirePoint.rotation);
+    }
+}
